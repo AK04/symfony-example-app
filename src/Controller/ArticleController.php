e7c215f800eae8f7091c65dd27805891dcc80ad9
Albin Kottaram <albindoesgames@gmail.com>
2019-06-30 17:03:33 +0530

    Added twig and article index template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,16 +4,21 @@
 
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
-    Class ArticleController {
+    Class ArticleController extends Controller {
 
         /**
         * @Route("/")
+        * @Method({"GET"})
         */
         public function index() {
 
-            return new Response("<html> Hello </html>");
+            //return new Response("<html> Hello </html>");
+
+            return $this->render("articles/index.html.twig");
 
         }
 
